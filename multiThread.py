f1fd5e5996560e6b7aee9236f046a7b9db2171f3
hsiang <chengshiang335@gmail.com>
2026-03-25 14:03:55 +0800

    多執行序

---
+++ multiThread.py
@@ -0,0 +1,25 @@
+from concurrent.futures import ThreadPoolExecutor
+
+def jobA():
+    for i in range(100):  
+     print("這是工作A")
+
+def jobB(): 
+    for i in range(100):     
+        print("這是工作B")        
+    
+def jobC():        
+    for i in range(100):     
+        print("這是工作C")  
+    
+def jobD():    
+    for i in range(100):  
+      print("這是工作D")  
+    
+#多工版本
+with ThreadPoolExecutor(max_workers=4) as executor:
+    executor.submit(jobA)
+    executor.submit(jobB)
+    executor.submit(jobC)
+    executor.submit(jobD)   
+        
